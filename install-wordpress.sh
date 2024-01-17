1b07edff600ecc6853a6fdd4c91046d9f614c45d
Samuel Combey <combeysamuel@gmail.com>
2024-01-17 16:17:54 -0500

    install wordpress script

--- install-wordpress.sh
+++ install-wordpress.sh
@@ -1,1 +1,45 @@
+# Create the html directory and mount the EFS to it
+sudo su
+yum update -y
+mkdir -p /var/www/html
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport <efs-id>:/ /var/www/html
+
+# Install Apache
+sudo yum install -y httpd httpd-tools mod_ssl
+sudo systemctl enable httpd 
+sudo systemctl start httpd
+
+# Install PHP 7.4
+sudo amazon-linux-extras enable php7.4
+sudo yum clean metadata
+sudo yum install php php-common php-pear -y
+sudo yum install php-{cgi,curl,mbstring,gd,mysqlnd,gettext,json,xml,fpm,intl,zip} -y
+
+# Install MySQL 5.7
+sudo rpm -Uvh https://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm
+sudo rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2022
+sudo yum install mysql-community-server -y
+sudo systemctl enable mysqld
+sudo systemctl start mysqld
+
+# Set permissions
+sudo usermod -a -G apache ec2-user
+sudo chown -R ec2-user:apache /var/www
+sudo chmod 2775 /var/www && find /var/www -type d -exec sudo chmod 2775 {} \;
+sudo find /var/www -type f -exec sudo chmod 0664 {} \;
+chown apache:apache -R /var/www/html 
+
+# Download WordPress files
+wget https://wordpress.org/latest.tar.gz
+tar -xzf latest.tar.gz
+cp -r wordpress/* /var/www/html/
+
+# Create the wp-config.php file
+cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+
+# Edit the wp-config.php file
+nano /var/www/html/wp-config.php
+
+# Restart the webserver
+service httpd restart
 
